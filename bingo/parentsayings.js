ed6a092fc0a2df2dbe1425ca5bfa9a01c62bf301
jpeterbaker <jonathanpeterbaker@gmail.com>
2018-10-18 13:26:24 -0400

    Today's update

---
+++ bingo/parentsayings.js
@@ -0,0 +1,33 @@
+var allsayings = [
+    1,"A story from the clinic",
+    1,"Amazement at how few vegetables they just ate",
+    1,"Amazement at how late they're up",
+    1,"Asking about Christmas plans",
+    1,"Complimenting the town",
+    1,"Concern about clinic business",
+    1,"Dad laughs when no one else does",
+    1,"Dad saying something mom ignores",
+    1,"How much mom loves fruit for breakfast",
+    1,"Insisting that they don't mind something",
+    1,"Insisting that they don't want to be a bother",
+    1,"Saying how nice Sarah looks",
+    1,"Story from dad's work",
+    1,"Struggling with technology",
+    1,"Suggesting having kids",
+    1,"Wanting to pay for something",
+    1,"Worrying about Christian",
+    1,"Worrying about Liz or Rachel",
+    1,"Worrying about Nathan",
+    1,"Worrying about Paul",
+    1,"Worrying about a nephew",
+    1,"\"How is the car running?\"",
+    1,"\"How long will you be here?\"",
+    1,"\"It's only X:00 for us\"",
+];
+/*
+Apple storage story
+A success of Nathan or Jessica
+A story from the clinic
+*/
+
+
