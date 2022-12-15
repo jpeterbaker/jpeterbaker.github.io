03ae57ceac02663040d215898a23323d9f2a56d6
jonpbak <jonpbak@vt.edu>
2022-12-14 23:57:35 -0500

    Capstone bingo

---
+++ bingo/capstone/sayings.js
@@ -0,0 +1,40 @@
+
+var TITLE = "Capstone Bingo";
+var BOARDTOP = "Capstone Bingo";
+var FREE = "Axis labels too small";
+var USEFREE = 1;
+var AUTOFREE = 0;
+
+var allsayings = [
+    1,'"Are there any questions?"',
+    1,'"Huge shoutout"',
+    1,'"Questions" slide',
+    1,'Bibliography slide',
+    1,'Blurry figure',
+    1,'Dashboard project',
+    1,'Hook involving sports',
+    1,'NLP project',
+    1,'Neural net project',
+    1,'Generic clipart',
+    1,'Pie chart',
+    1,'Reading a full line of text',
+    1,'Underscores in figure',
+    1,'Unlabeled axis',
+    1,'Using remote computing resources',
+    1,'Word cloud',
+    1,'Advancing ',
+    1,'Multicolor text',
+    1,'Unexplained abbreviation',
+    1,'Going over time',
+    1,'K-means',
+    1,'LASSO regression',
+    1,'Python',
+    1,'K-fold validation',
+    1,'R',
+    1,'MATLAB',
+    1,'Laughter',
+    1,'PCA',
+    1,'XGBoost',
+    1,'SQL',
+];
+
