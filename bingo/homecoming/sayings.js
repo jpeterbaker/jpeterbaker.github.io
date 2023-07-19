b9fb4582e78867737ecff6cc15db0d43e529bf39
jonpbak <jonpbak@vt.edu>
2023-07-19 12:16:18 -0600

    Tournament summaries, quick rule edits

---
+++ bingo/homecoming/sayings.js
@@ -0,0 +1,37 @@
+var TITLE = "Huntsville Smalltalk Bingo 2023";
+var BOARDTOP = "Huntsville Smalltalk Bingo 2023";
+var FREE = "How long are you staying";
+var USEFREE = 1;
+var AUTOFREE = 0;
+
+var allsayings = [
+    2,"Do you have kids",
+    2,"What are you doing now",
+    2,"Do you have a job lined up",
+    2,"You look just like your father",
+    2,"Where have you been",
+    2,"How long were you there",
+    1,"Were you here for the 4th",
+    2,"How long have you been married",
+    2,"Are you enjoying Huntsville",
+    2,"Do you remember me",
+    2,"Where is Etta",
+    2,"How is Paul",
+    2,"How was Virginia",
+    2,"Good things are just around the corner",
+    2,"You'll have a great time here",
+    2,"Where are you from",
+    2,"Remind me of your name",
+    2,"Have you published anything",
+    2,"What was your research about",
+    2,"Did you fly or drive",
+    2,"Where are you staying",
+    2,"How did you meet",
+    2,"Where are you going next",
+    2,"What are you doing next",
+    2,"Are you going to teach",
+    2,"Did you have much stuff",
+    1,"The heat",
+    1,"The flooding",
+];
+
